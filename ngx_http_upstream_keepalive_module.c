8e17b31b972756acc1c319c7386534c4a44bee59
Maxim Dounin <mdounin@mdounin.ru>
2009-06-02 21:53:19 +0400

    Keepalive: fix create_conf to return NULL on errors.

--- ngx_http_upstream_keepalive_module.c
+++ ngx_http_upstream_keepalive_module.c
@@ -409,7 +409,7 @@ ngx_http_upstream_keepalive_create_conf(ngx_conf_t *cf)
     conf = ngx_pcalloc(cf->pool,
                        sizeof(ngx_http_upstream_keepalive_srv_conf_t));
     if (conf == NULL) {
-        return NGX_CONF_ERROR;
+        return NULL;
     }
 
     /*
